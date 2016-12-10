3d4dbd8f92cfce26b4f07624067c3c1d8c827497
Riki Rokhman Azis <riki.rokhman.azis@gmail.com>
2016-12-11 00:24:23 +0700

    avatar view blade

---
+++ resources/views/tnde/users-avatar-list.blade.php
@@ -0,0 +1,10 @@
+															  <ul>
+                                                                <li>
+                                                                <p>
+                                                                <div class="mt-comment-img">
+                                                                    <img src="{{ $userdb->avatar }}" height="50px"/> 
+                                                                </div>
+                                                                </p>
+                                                                <!--<a href="/attachment-show-incoming/{{ $userdb->id }}">{{ $userdb->avatar }}</a>&nbsp; -->
+                                                                </li>
+                                                              </ul>
